7d18d4baa5e3b7d786e19b21b90d46ea026cb6b4
Diddi <dps.dtt25@gmail.com>
2023-10-10 20:38:10 +0200

    kan inte gå vidare med first task

--- src/SingleList.java
+++ src/SingleList.java
@@ -1,41 +1,42 @@
 public class SingleList<T extends Comparable<T>> {
     Cell<T> firstInFirstImplementation;
     Cell<T> firstInSecondImplementation;
-    Cell<T> lastInSecondImplementation;
 
     public void addInConstantTime(T item) {
         if (this.firstInFirstImplementation == null) {
             firstInFirstImplementation = new Cell<>(item, null);
         } else {
-            // går inte utan att springa igenom + sortering
+            // går inte utan att springa igenom + sortering, dessutom måste ha lastpekare
         }
     }
-    
-    public void removeNTime(T item) {
+    public Cell<T> removeNTime() {
         Cell<T> temporary = this.firstInFirstImplementation;
         while(temporary != null) {
-            if (temporary.nextCell.item.equals(item)) {
-                temporary.nextCell = temporary.nextCell.nextCell;
-                break;
+            if (temporary.nextCell.nextCell != null) {
+                Cell<T> saveBeforeReturn = temporary.nextCell;
+                temporary.nextCell = null;
+                return saveBeforeReturn;
             }
             temporary = temporary.nextCell;
         }
+        return null;
     }
     
     public void addNTime(T item) {
-        if (this.firstInSecondImplementation != null) {
+        if (this.firstInSecondImplementation == null) {
             this.firstInSecondImplementation = new Cell<>(item, null);
         } else {
-            Cell<T> temporary = this.lastInSecondImplementation;
+            Cell<T> temporary = this.firstInSecondImplementation;
             while (temporary.nextCell != null) {
                 temporary = temporary.nextCell;
             }
-            temporary.nextCell = new Cell<>(item, null);
+            temporary.nextCell = new Cell<>(item,null); //fortfarande inte sorterad
         }
     }
-
-    public void removeInConstantTime(T item) {
-
+    public Cell<T> removeInConstantTime() {
+        Cell<T> saveBeforeReturn = this.firstInSecondImplementation;
+        this.firstInSecondImplementation.nextCell = this.firstInSecondImplementation;
+        return saveBeforeReturn;
     }
 
 }
