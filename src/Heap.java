cb6580a971999d4dcf6ff2c9d44f112f39b40b4e
Diddi <diddi.dutta@outlook.com>
2023-10-11 14:02:04 +0200

    förhoppningsvis klar med addInNodeClass()

--- src/Heap.java
+++ src/Heap.java
@@ -4,17 +4,15 @@ public class Heap<T extends Comparable<T>> {
         if (this.root == null) {
             this.root = new Node<>(item, 0);
         } else {
-            addInAnother(item);
+            this.root.addInNodeClass(item);
         }
-        Node<T> traverseNode = this.root;
-        //om variabel = true => left bransch, sedan ska vi kolla om det har en equals, sedan
-        //om variabel = false => right branch
-    }
-    public void addInAnother(T item) {
-
     }
     public void remove() {
-
+        if (this.root == null) {
+            return;
+        } else {
+            this.root.addInNodeClass(item);
+        }
     }
 
 }
--- src/Node.java
+++ src/Node.java
@@ -3,10 +3,45 @@ public class Node<T extends Comparable<T>> implements Comparable <T> {
     Integer numberOfNodes;
     Node<T> right;
     Node<T> left;
+    boolean leftOrRightDecision = true;
     public Node(T item, Integer numberOfNodes) {
         this.item = item;
         this.numberOfNodes = numberOfNodes;
         this.left = this.right = null;
+    }
+    public void addInNodeClass(T newItem) {
+        if (this.item.equals(item)) {
+            return;
+        }
+        if (leftOrRightDecision) {
+            if (this.left == null) {
+                this.left = new Node<>(newItem, this.numberOfNodes++);
+                changeDirectionDecision();
+            } else if (this.left.item.compareTo(item) > 0) {
+                T newItemToAdd = swapItems(newItem);
+                this.left.addInNodeClass(newItemToAdd);
+            } else {
+                this.left.addInNodeClass(newItem);
+            }
+        } else {
+            if (this.right == null) {
+                this.right = new Node<>(newItem, this.numberOfNodes++);
+                changeDirectionDecision();
+            } else if (this.right.item.compareTo(item) > 0) {
+                T newItemToAdd = swapItems(newItem);
+                this.right.addInNodeClass(newItemToAdd);
+            } else {
+                this.right.addInNodeClass(newItem);
+            }
+        }
+    }
+    private T swapItems(T newItem) {
+        T savedItem = this.item;
+        this.item = newItem;
+        return savedItem;
+    }
+    private void changeDirectionDecision() {
+        leftOrRightDecision = !leftOrRightDecision;
     }
     @Override
     public int compareTo(T item) {
