16514bc653ea3475c50bf1272dad1e65d3825365
Diddi <diddi.dutta@outlook.com>
2023-10-11 13:06:59 +0200

    försök göra detta utanför Nodeklassen

--- src/Heap.java
+++ src/Heap.java
@@ -4,11 +4,14 @@ public class Heap<T extends Comparable<T>> {
         if (this.root == null) {
             this.root = new Node<>(item, 0);
         } else {
-            Node<T> traverseNode = this.root;
-            if (traverseNode.compareTo(item) < 0) {
-
-            }
+            addInAnother(item);
         }
+        Node<T> traverseNode = this.root;
+        //om variabel = true => left bransch, sedan ska vi kolla om det har en equals, sedan
+        //om variabel = false => right branch
+    }
+    public void addInAnother(T item) {
+
     }
     public void remove() {
 
